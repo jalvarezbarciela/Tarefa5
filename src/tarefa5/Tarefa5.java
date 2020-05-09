adea49aab09216c249487f730ca43474b0442b03
jalvarezbarciela <jalvarezbarciela@danielcastelao.org>
2020-05-09 15:40:52 +0200

    Commit 1 Tarefa 5

---
+++ src/tarefa5/Tarefa5.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarefa5;
+
+/**
+ *
+ * @author usuario
+ */
+public class Tarefa5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora c;
+        c=new Calculadora();
+        c.setVisible(true);
+    }
+    
+}
